08ae9b34fb1d5e584f9931898fe9966b41026a7d
odoroliver <71766082+odoroliver@users.noreply.github.com>
2022-11-17 11:27:28 +0100

    Add files via upload

---
+++ Tower_Defense/tilemap.py
@@ -0,0 +1,29 @@
+import pygame as pg
+import pytmx
+import os
+from settings import *
+vec = pg.math.Vector2
+
+
+class TiledMap:
+  def __init__(self, fajlnev):
+    tm = pytmx.load_pygame(fajlnev, pixelalpha=True)
+    self.width = tm.width * tm.tilewidth
+    self.height = tm.height * tm.tileheight
+    self.tmxdata = tm
+
+  def render(self, surface):
+    ti = self.tmxdata.get_tile_image_by_gid
+    for layer in self.tmxdata.visible_layers:
+        if isinstance(layer, pytmx.TiledTileLayer):
+          for x, y, gid, in layer:
+            tile = ti(gid)
+            if tile:
+                surface.blit(tile, (x * self.tmxdata.tilewidth,
+                                    y * self.tmxdata.tileheight))
+  def map_keszites(self):
+    temp_surface = pg.Surface((self.width, self.height))
+    self.render(temp_surface)
+    return temp_surface
+        
+        
